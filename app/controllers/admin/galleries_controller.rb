647f5fac7027152e238444d6a1489ac568069d4a
Levi McPhetridge <levimcphetridge@gmail.com>
2013-11-06 00:55:00 -0500

    many updates to how galleries work

---
+++ app/controllers/admin/galleries_controller.rb
@@ -0,0 +1,40 @@
+class Admin::GalleriesController < AdminController
+  def index
+    @galleries = Gallery.all
+  end
+  
+  
+  def new
+     @gallery = Gallery.new
+     @gallery.name = 'New Gallery'
+     
+     respond_to do |format|
+       if @gallery.save
+         format.js    
+       end
+       
+     end     
+   end
+   
+   def create
+     @gallery = Gallery.new(params[:post])
+    
+     if @image.save
+       redirect_to @gallery
+     else
+       render 'new'
+     end
+       
+   end
+   
+   def destroy
+     @gallery = Gallery.find(params[:id])
+     @gallery.destroy
+     
+     respond_to do |format|
+      format.js 
+     end
+     
+   end
+
+end
