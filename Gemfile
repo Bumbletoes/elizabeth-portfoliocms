fd2d0fd45cacf5e4a57f755e976c909f5820061d
Levi <Levi@THEMONSTER>
2013-11-21 23:51:13 -0800

    more updates to image uplaod stuff

--- Gemfile
+++ Gemfile
@@ -16,7 +16,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 
 
-gem 'bootstrap-sass', '~> 3.0.1.0.rc'
+gem 'bootstrap-sass', '~> 3.0.2.0'
 
 gem 'font-awesome-sass'
 
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,4 @@
 class Image < ActiveRecord::Base
   belongs_to :gallery
-  has_attached_file :photo, :default_url => "/images/:style/missing.png"
+  has_attached_file :photo, :default_url => "/images/no-image.jpg"
 end
