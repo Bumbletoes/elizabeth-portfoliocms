e7e919dc0613bdc955edb9e86dd81e9a069b5ca5
Levi <Levi@THEMONSTER>
2013-11-22 00:53:28 -0800

    delete function complete for images

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,7 @@
 class Image < ActiveRecord::Base
   belongs_to :gallery
   has_attached_file :photo, :default_url => "/images/no-image.jpg"
+  
+  # add this back in to support resizing
+  #:styles => { :medium => "300x300", :thumbnail => "100x100" }, 
 end
