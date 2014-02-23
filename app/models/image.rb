bd0c28a2b5ccccbd1191c420ad72783df405c1dd
Levi <levimcphetridge@gmail.com>
2014-02-23 00:03:19 -0500

    testing upload directory again

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,8 +2,8 @@ class Image < ActiveRecord::Base
   belongs_to :gallery
   has_attached_file :photo, :styles => {:medium => "300x300>", :thumb => "100x100>"}, 
       :default_url => "./assets/no-image.jpg",
-      :path => ":rails_root/public/system/:attachment/:id/:basename_:style.:extension",
-      :url => "/system/:attachment/:id/:basename_:style.:extension",
+      :path => ":rails_root/app/assets/images/:attachment/:id/:basename_:style.:extension",
+      :url => "/app/assets/images/:id/:basename_:style.:extension",
 
 
 
