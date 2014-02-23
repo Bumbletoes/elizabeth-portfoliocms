a62cde2cb417be2b5b755ab6692a554e45cfe68d
Levi <levimcphetridge@gmail.com>
2014-02-22 23:46:11 -0500

    changing up where images are stored, hoping defaults will work

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,9 +1,8 @@
 class Image < ActiveRecord::Base
   belongs_to :gallery
   has_attached_file :photo, :styles => {:medium => "300x300>", :thumb => "100x100>"}, 
-      :default_url => "./assets/no-image.jpg",
-      :url => "/assets/images/:id/:style/:basename.:extension",
-      :path => ":rails_root/public/assets/images/:id/:style/:basename.:extension"
+      :default_url => "./assets/no-image.jpg"
+
 
   validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 5.megabytes
