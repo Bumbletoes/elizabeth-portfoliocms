e5eaf3d38f8e51b3b5af43653feeb3ed4f14b589
Levi <levimcphetridge@gmail.com>
2014-02-24 01:48:12 -0500

    gahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module ElizabethPortfolio
   class Application < Rails::Application
       config.filter_parameters += [:password]
       config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+      config.assets.precompile += %w(admin)
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
