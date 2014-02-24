0d68428f805efdea156046ba444eb2d1075f69b0
Levi <levimcphetridge@gmail.com>
2014-02-24 01:10:10 -0500

    gahhhhh\! assets permissions/precompilation fail

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,8 @@ Bundler.require(:default, Rails.env)
 
 module ElizabethPortfolio
   class Application < Rails::Application
-    config.filter_parameters += [:password]
-    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+      config.filter_parameters += [:password]
+      config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
