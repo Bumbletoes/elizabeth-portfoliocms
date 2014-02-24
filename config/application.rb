1a4a5004021f1a70b2454e4f7326a95d53617874
Levi <levimcphetridge@gmail.com>
2014-02-24 00:14:46 -0500

    adding styles to front page and adding some fonts

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(:default, Rails.env)
 module ElizabethPortfolio
   class Application < Rails::Application
     config.filter_parameters += [:password]
+    config.assets.paths << Rails.root.join('app','assets','fonts')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
