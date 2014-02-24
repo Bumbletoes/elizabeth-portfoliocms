c84ec947bd44f2470be81cb1853cd648c6d860c2
Levi <levimcphetridge@gmail.com>
2014-02-23 22:33:11 -0500

    forget it\!

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,10 +29,7 @@ set :ssh_options, { :forward_agent => true }
 namespace :deploy do
 
   desc 'Restart application'
-  task :restart, :roles => :app, :except => { :no_release => true } do
-      # Hack to have capistrano enter the sudo password (for rvmsudo later)
-      sudo "whoami"
-      execute "rvmsudo /etc/init.d/thin restart"
+  task :restart  do
   end
 
   after :restart, :clear_cache do
