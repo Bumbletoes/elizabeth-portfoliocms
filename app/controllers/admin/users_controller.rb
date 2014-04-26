3cf06932b943ee4f871109f3218cc740a13ddea6
Levi <levimcphetridge@gmail.com>
2014-04-26 11:34:57 -0400

    making about me save

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,18 @@ class Admin::UsersController < AdminController
     def index
         @user = User.find_by(:username => 'levi')
     end
+
+    def update 
+        @user = User.find_by(:username => 'levi')
+
+        if @user.update_attribute(:about_me, user_params[:about_me])
+            redirect_to admin_galleries_url
+        end
+    end
+   
+    private
+    def user_params
+        params.require(:user).permit(:about_me)
+    end
+
 end
