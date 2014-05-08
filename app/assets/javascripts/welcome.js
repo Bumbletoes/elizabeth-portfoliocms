37bd98525721f296b91dca3fa51865de26a6611c
Levi <levimcphetridge@gmail.com>
2014-05-07 22:50:32 -0400

    testing out jumping to images

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,8 +1,8 @@
 $(document).on('ready page:load',function(){
     $('#header').click(function(e){
         if(e.toElement.id == 'header'){
-            var position = -60;
-            if($('#header').css('bottom') == '-60px'){
+            var position = -70;
+            if($('#header').css('bottom') == '-70px'){
                 position = 50;
             }
             $('#header').animate({bottom: position},"fast");
@@ -13,5 +13,9 @@ $(document).on('ready page:load',function(){
         url: 'load_gallery/2',
         type: 'GET',
         dataType: 'script'
-    });   
+    }).done(function(){
+        $('.preview-image-container img').click(function(){
+            api.goTo($(this).attr('data-index'));
+        });
+    });
 });
