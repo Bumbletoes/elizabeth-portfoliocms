9d27c81e6bf10b816a3f3fe9da4be9ee9a6db76f
Levi <levimcphetridge@gmail.com>
2014-05-07 21:48:24 -0400

    got hit area working

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,10 +1,14 @@
 $(document).on('ready page:load',function(){
     $('#hit-area').hover(function(e){
         var position = -60;
+        var hitAreaPosition = -110;
         if($('#header').css('bottom') == '-60px'){
             position = 50;
+            hitAreaPosition = 0;
         }
         $('#header').animate({bottom: position},"fast");
+        $('#hit-area').animate({bottom: hitAreaPosition},"fast");
+
     });
 
     $.ajax({
