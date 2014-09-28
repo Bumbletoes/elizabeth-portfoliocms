35c3597e0b697a79593d8dacecadbf6f44365391
Levi McPhetridge <levi.mcphetridge@pgi.com>
2014-09-27 23:31:47 -0400

    turning off the animating header on click

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,13 +1,13 @@
 $(document).on('ready page:load',function(){
-    $('#header').click(function(e){
-        if(e.toElement.id == 'header'){
-            var position = -70;
-            if($('#header').css('bottom') == '-70px'){
-                position = 50;
-            }
-            $('#header').animate({bottom: position},"fast");
-        }
-    });
+   // $('#header').click(function(e){
+   //     if(e.toElement.id == 'header'){
+   //         var position = -70;
+   //         if($('#header').css('bottom') == '-70px'){
+   //             position = 50;
+   //         }
+   //         $('#header').animate({bottom: position},"fast");
+   //     }
+   //  });
 
     $.ajax({
         url: 'load_gallery/6',
