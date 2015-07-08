8a19807e00da365922a5b771d4061f7a1a4aea9b
Levi McPhetridge <levimcphetridge@gmail.com>
2015-07-08 02:25:41 +0000

    welcome.js  -- killing commented out code with fire

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,14 +1,4 @@
 $(document).on('ready page:load',function(){
-   // $('#header').click(function(e){
-   //     if(e.toElement.id == 'header'){
-   //         var position = -70;
-   //         if($('#header').css('bottom') == '-70px'){
-   //             position = 50;
-   //         }
-   //         $('#header').animate({bottom: position},"fast");
-   //     }
-   //  });
-
     $.ajax({
         url: 'load_gallery/6',
         type: 'GET',
